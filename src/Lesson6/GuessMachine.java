a54f09042bce159f0d6d9fd337099dba40e5bcda
hari9160 <hari9160@ISS2V2RKH2.scdsb.pri>
2019-10-10 12:00:08 -0400

    Oct 3, 2019 Commit

---
+++ src/Lesson6/GuessMachine.java
@@ -0,0 +1,10 @@
+package Lesson6;
+
+public class GuessMachine {
+    //Step 1: Instance Variables: 
+    private int number; 
+    private int numGuesses; 
+    private int guess; 
+    
+    //Step 2: 
+}
